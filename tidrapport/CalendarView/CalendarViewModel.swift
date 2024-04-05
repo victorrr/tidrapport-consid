6451b011b22e754e6b52582b763ff923a94487a2
Victor Rendon <victor.rendon.rapp@gmail.com>
2024-04-05 10:59:41 +0200

    Create view month view models

--- tidrapport/CalendarView/CalendarViewModel.swift
+++ tidrapport/CalendarView/CalendarViewModel.swift
@@ -7,28 +7,27 @@
 
 import Foundation
 
-struct CalendarViewModel {
+final class CalendarViewModel {
     var year: Int
-    private var calendar: Calendar {
-        var cal = Calendar(identifier: .gregorian)
-        cal.locale = Locale(identifier: "en_GB") // Monday is considered the first day of the week in the UK
-        return cal
+    lazy var monthViewModels: [MonthViewModel] = (1..<13).compactMap(createMonthViewModel)
+
+    init(year: Int) {
+        self.year = year
     }
 
     func monthViewModel(_ monthNumber: Int) -> MonthViewModel {
-        let month = calendar.date(from: DateComponents(year: year, month: monthNumber)) ?? Date()
-        return MonthViewModel(month: month)
+        monthViewModels[monthNumber-1]
     }
-    
-    func dataForYear(_ year: Int) -> [CalendarData] {
-        var calendarDataList = [CalendarData]()
+
+    func dataForYear(_ year: Int) -> [ProjectData] {
+        var calendarDataList = [ProjectData]()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy/MM/dd"
         let startDate = dateFormatter.date(from: "\(year)/01/01")!
         let endDate = dateFormatter.date(from: "\(year)/12/31")!
         for date in DateInterval(start: startDate, end: endDate).days {
             let dateString = dateFormatter.string(from: date)
-            let calendarData = CalendarData(id: "0",
+            let calendarData = ProjectData(id: "0",
                                             date: dateString,
                                             hours: "",
                                             project: "",
@@ -38,5 +37,20 @@ struct CalendarViewModel {
             calendarDataList.append(calendarData)
         }
         return calendarDataList
+    }
+}
+
+// MARK: - Private
+
+private extension CalendarViewModel {
+
+    var calendar: Calendar {
+        var cal = Calendar(identifier: .gregorian)
+        cal.locale = Locale(identifier: "en_GB") // Monday is considered the first day of the week in the UK
+        return cal
+    }
+
+    func createMonthViewModel(_ monthNumber: Int) -> MonthViewModel? {
+        calendar.date(from: DateComponents(year: year, month: monthNumber)).map { MonthViewModel(month: $0) }
     }
 }
