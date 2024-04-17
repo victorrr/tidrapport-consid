46881c94455ca711d57528653c739cf5f9ef6084
Victor Rendon <victor.rendon.rapp@gmail.com>
2024-04-17 14:40:45 +0200

    Lägger till navigationsbar med stäng-knapp
    Detta commit lägger till en navigationsbar till AddTimeView, inklusive en stäng-knapp. Knappen stänger vyn när den klickas på.

--- tidrapport/AddTimeView/AddTimeView.swift
+++ tidrapport/AddTimeView/AddTimeView.swift
@@ -9,33 +9,44 @@ import SwiftUI
 
 struct AddTimeView: View {
     @ObservedObject var viewModel: AddTimeViewModel
+    @State private var path: NavigationPath = NavigationPath()
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        VStack {
-            Text("Lägg till nya tider")
-                .font(.title)
-                .padding(.top, 8)
-            Form {
-                Section(header: Text("Valda datum")) {
-                    List(viewModel.days, id: \.self) { day in
-                        Text(day)
+        NavigationStack(path: $path) {
+            VStack {
+                Form {
+                    Section(header: Text("Valda datum")) {
+                        List(viewModel.days, id: \.self) { day in
+                            Text(day)
+                        }
+                    }
+                    Section {
+                        TextField("Timmar", value: $viewModel.hours, formatter: NumberFormatter())
+                        TextField("Artikel", text: $viewModel.article)
+                        TextField("Kund", text: $viewModel.customer)
+                        TextField("Projekt", text: $viewModel.project)
+                        TextField("Aktivitet", text: $viewModel.activity)
+                        TextField("Ärendenummer", text: $viewModel.errandNumber)
+                        TextField("Beskrivning", text: $viewModel.description)
+                    }
+                    NavigationLink {
+                        viewModel.saveProjectData()
+                        return SubmitView(projectData: viewModel.projectData,
+                                          dates: viewModel.selectedDates)
+                    } label: {
+                        Text("Spara")
                     }
                 }
-                Section {
-                    TextField("Timmar", value: $viewModel.hours, formatter: NumberFormatter())
-                    TextField("Artikel", text: $viewModel.article)
-                    TextField("Kund", text: $viewModel.customer)
-                    TextField("Projekt", text: $viewModel.project)
-                    TextField("Aktivitet", text: $viewModel.activity)
-                    TextField("Ärendenummer", text: $viewModel.errandNumber)
-                    TextField("Beskrivning", text: $viewModel.description)
-                }
-                NavigationLink {
-                    viewModel.saveProjectData()
-                    return SubmitView(projectData: viewModel.projectData,
-                                      dates: viewModel.selectedDates)
-                } label: {
-                    Text("Spara")
+            }
+            .navigationTitle("Lägg till tid")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Stäng")
+                    }
                 }
             }
         }
--- tidrapport/Calendar/CalendarView/CalendarView.swift
+++ tidrapport/Calendar/CalendarView/CalendarView.swift
@@ -9,21 +9,19 @@ import SwiftUI
 
 struct CalendarView: View {
     var viewModel = CalendarViewModel()
-    @State private var isButtonPressed: Bool = false
+    @State private var isPresentingAddTimeView: Bool = false
 
     var body: some View {
-        NavigationStack {
-            GeometryReader { geometry in
-                ZStack {
-                    List(1..<13, id: \.self) { monthNumber in
-                        MonthView(viewModel: viewModel.monthViewModel(monthNumber))
-                    }
-                    addTimesButton(x: geometry.size.width - 80, 
-                                   y: geometry.size.height - 80)
+        GeometryReader { geometry in
+            ZStack {
+                List(1..<13, id: \.self) { monthNumber in
+                    MonthView(viewModel: viewModel.monthViewModel(monthNumber))
                 }
-                .navigationDestination(for: Int.self, destination: { dates in
-                    addTimeView
-                })
+                addTimesButton(x: geometry.size.width - 80,
+                               y: geometry.size.height - 80)
+            }
+            .sheet(isPresented: $isPresentingAddTimeView) {
+                addTimeView
             }
         }
     }
@@ -38,12 +36,14 @@ private extension CalendarView {
         addTimeViewModel.prefillProjectData()
         return AddTimeView(viewModel: addTimeViewModel)
             .onDisappear {
-                isButtonPressed = false
+                isPresentingAddTimeView = false
             }
     }
 
     func addTimesButton(x: CGFloat, y: CGFloat) -> some View {
-        NavigationLink(value: 0) {
+        Button {
+            isPresentingAddTimeView.toggle()
+        } label: {
             Image(systemName: "plus.circle.fill")
                 .resizable()
                 .frame(width: 40, height: 40)
